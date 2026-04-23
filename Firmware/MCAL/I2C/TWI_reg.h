09ef0272a6bcd52b80d8edafb580edf5e6252d97
la-ines <laribi.ines12@gmail.com>
2026-04-23 17:21:59 +0100

    Update TWI_reg.h

--- Firmware/MCAL/I2C/TWI_reg.h
+++ Firmware/MCAL/I2C/TWI_reg.h
@@ -1,12 +1,12 @@
 #ifndef TWI_REG_H_
 #define TWI_REG_H_
 
-#define TWBR_Reg        *((volatile uint8_t*)0xB8) //sets SCL frequency (speed of I2C)
-#define TWSR_Reg       *((volatile uint8_t*)0xB9) //shows current TWI status (start,ACK,STOP..)
-#define TWAR_Reg        *((volatile uint8_t*)0xBA) //sets the slave mode (only used when atmega is the slave)
-#define TWDR_Reg         *((volatile uint8_t*)0xBB) // holds data to be transmitted or received
-#define TWCR_Reg         *((volatile uint8_t*)0xBC) //main control register
-#define TWAMR_Reg          *((volatile uint8_t*)0xBD)  //address matching in slave mode
+#define TWBR_Reg        *((volatile uint8_t*)0xB8) 
+#define TWSR_Reg       *((volatile uint8_t*)0xB9) 
+#define TWAR_Reg        *((volatile uint8_t*)0xBA) 
+#define TWDR_Reg         *((volatile uint8_t*)0xBB) 
+#define TWCR_Reg         *((volatile uint8_t*)0xBC) 
+#define TWAMR_Reg          *((volatile uint8_t*)0xBD)  
 
 
 
